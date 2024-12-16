3b72167c9687d6a27ae1c55a06f86ba62d54dde0
Ali Reda <alyredagomaa@gmail.com>
2024-12-16 19:58:11 +0200

    send notification also

--- script.js
+++ script.js
@@ -3,16 +3,33 @@ const timerParagraph = document.getElementById("timer");
 let timer;
 let currentMinute;
 let currentSecond;
+var notificationGranted = false;
 
 button.addEventListener("click", () => {
+  if ("Notification" in window) {
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        notificationGranted = true;
+      } else {
+        console.log("Notification permission denied.");
+      }
+    });
+  } else {
+    alert("Browser does not support notifications.");
+  }
+
   clearInterval(timer);
-  
+
   timer = setInterval(() => {
     currentMinute = new Date().getMinutes();
     currentSecond = new Date().getSeconds();
 
-    timerParagraph.innerText = "HH:" + (currentMinute > 9 ? currentMinute : "0" + currentMinute) + ":" + (currentSecond > 9 ? currentSecond : "0" + currentSecond);
-    if (currentMinute%20 === 0 && currentSecond > 0 && currentSecond <= 10) {
+    timerParagraph.innerText =
+      "HH:" +
+      (currentMinute > 9 ? currentMinute : "0" + currentMinute) +
+      ":" +
+      (currentSecond > 9 ? currentSecond : "0" + currentSecond);
+    if (currentMinute % 20 === 0 && currentSecond > 0 && currentSecond <= 10) {
       if (currentMinute === 0) {
         playSound("sound2.mp3");
       } else {
@@ -20,9 +37,15 @@ button.addEventListener("click", () => {
       }
     }
   }, 1000);
-})
+});
 
 function playSound(file) {
   const audio = new Audio(file);
   audio.play().catch((error) => console.log("Sound playback error:", error));
+
+  if (notificationGranted) {
+    new Notification("Time's up!", {
+      body: "20 minutes have passed!",
+    });
+  }
 }
